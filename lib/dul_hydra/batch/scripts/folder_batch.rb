b693b963a2c3bcd105a83de8bc0e9afd530e9f30
Jim Coble <jim.coble@duke.edu>
2013-10-18 08:52:15 -0400

    initial work on FolderBatch script

---
+++ lib/dul_hydra/batch/scripts/folder_batch.rb
@@ -0,0 +1,44 @@
+module DulHydra::Batch::Scripts
+  class FolderBatch
+   
+    FILE_OBJECT_MODEL = 'Component'
+   
+    # Options
+    #   :dirpath - required - path to directory tree containing files to be ingested
+    #   :username - required - username of user with whom to associate batch
+    
+    def initialize(opts={})
+      @dirpath = opts.fetch(:dirpath)
+      @username = opts.fetch(:username)
+    end
+    
+    def execute
+      create_batch(User.find_by_username(@username))
+      process_files(@dirpath)
+    end
+    
+    def process_files(dirpath)
+      Dir.foreach(dirpath) do |entry|
+        unless [".", ".."].include?(entry)
+          if File.directory?(File.join(dirpath, entry))
+            process_files(File.join(dirpath, entry))
+          else
+            create_batch_object_for_file(entry)
+          end
+        end
+      end      
+    end
+    
+    def create_batch_object_for_file(file_entry)
+      obj = DulHydra::Batch::Models::IngestBatchObject.new(
+              :batch => @batch,
+              :model => FILE_OBJECT_MODEL
+              )
+    end
+
+    def create_batch(user)
+      @batch = DulHydra::Batch::Models::Batch.create(:user => user)
+    end
+    
+  end
+end
